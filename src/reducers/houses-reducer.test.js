27444cf3e047cf0c8b453babafa33535726a830d
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-04 11:24:29 -0700

    write tests for houses reducer

---
+++ src/reducers/houses-reducer.test.js
@@ -0,0 +1,17 @@
+import {houses} from './houses-reducer';
+import * as actions from '../actions';
+
+describe('housesReducer', () => {
+  it('should return an empty array by default', () => {
+    const expected = [];
+    const fakeAction = {type: ''};
+
+    expect(houses(undefined, fakeAction)).toEqual(expected);
+  });
+
+  it('should return houses when setHouses action is dispatched', () => {
+    const houseArray = ['an', 'array', 'of', 'houses'];
+    
+    expect(houses(undefined, actions.setHouses(houseArray))).toEqual(houseArray);
+  });
+});
