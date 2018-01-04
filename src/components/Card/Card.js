1734dae6ef778f8cb43a77a640201f228a5723aa
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-04 10:12:33 -0700

    add very basic styling to Card and Card Container

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './Card.css';
 
 const Card = ({ house }) => {
   const {name, founded, seats, titles, coatOfArms, ancestralWeapons, words} = house;
+
+  const displayFounded = founded ? founded : 'N/A';
 
   return (
     <div className="card">
       <h3>{name}</h3>
       <h4>{words}</h4>
-      <h4>Founded: {founded}</h4>
+      <h4>Founded: {displayFounded}</h4>
       <p>Seats: {seats}</p>
       <p>Titles: {titles}</p>
       <p>Ancestral Weapons: {ancestralWeapons}</p>
--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '../Card/Card';
 import { connect } from 'react-redux';
+import './CardContainer.css';
 
 const CardContainer = ({ houses }) => {
   const cards = houses.map(house => <Card house={house} />);
